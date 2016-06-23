f45dbcdb37a21e1d2b13279d1a010dfa22598514
jslim18 <jslim18@gmail.com>
2016-06-24 00:04:26 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -627,7 +627,7 @@ dp.add_handler(CommandHandler('download_database', download_db))
 dp.add_handler(CommandHandler('cancel', cancel))
 dp.add_handler(MessageHandler([Filters.text, Filters.photo, Filters.document],
                              message_handler))
-dp.add_ErrorHandler(error)
+dp.add_error_handler(error)
 
 start_bot(u)
 u.idle()
